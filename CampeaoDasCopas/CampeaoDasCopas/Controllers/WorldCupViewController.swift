8cd8c080575c38c9924bb371c0b5130451afad2a
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-02-02 21:34:53 -0300

    initial settings
    Iniciando as configurações iniciais do app.

---
+++ CampeaoDasCopas/CampeaoDasCopas/Controllers/WorldCupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WorldCupViewController.swift
+//  CampeaoDasCopas
+//
+//  Created by Rennan Bruno on 02/02/22.
+//
+
+import UIKit
+
+class WorldCupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CampeaoDasCopas/CampeaoDasCopas/Models/WorldCup.swift
@@ -0,0 +1,8 @@
+//
+//  WorldCup.swift
+//  CampeaoDasCopas
+//
+//  Created by Rennan Bruno on 02/02/22.
+//
+
+import Foundation
