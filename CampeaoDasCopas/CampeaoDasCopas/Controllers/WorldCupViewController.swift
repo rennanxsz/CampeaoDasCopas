bbbdc4d65ed2b1f0ef408da1b566f966eb6b4cc6
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-02-03 22:53:02 -0300

    Initializing worldCup view controller configuration
    Iniciando as configurações da tela de informações da copa.

--- CampeaoDasCopas/CampeaoDasCopas/Controllers/WorldCupViewController.swift
+++ CampeaoDasCopas/CampeaoDasCopas/Controllers/WorldCupViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 class WorldCupViewController: UIViewController {
     
     var worldCup: WorldCup!
-
+    
+    @IBOutlet weak var ivWinner: UIImageView!
+    @IBOutlet weak var ivVice: UIImageView!
+    @IBOutlet weak var lbScore: UILabel!
+    @IBOutlet weak var lbWinner: UILabel!
+    @IBOutlet weak var lbVice: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print ("Ano da copa selecionado: \(worldCup.year)")
+        title = "WorldCup \(worldCup.year)"
+        ivWinner.image = UIImage(named: "\(worldCup.winner).png")
+        ivVice.image = UIImage(named: "(\(worldCup.vice).png")
+        lbWinner.text = worldCup.winner
+        lbVice.text = worldCup.vice
+        lbScore.text = "\(worldCup.winnerScore) x \(worldCup.viceScore)"
     }
     
 
